ac724c1bac2d3b47cb70b5dde5a079d387cfadbd
Esra-Turk <esra.turk@bil.omu.edu.tr>
2022-12-20 19:42:17 +0300

    created selectLocation method and updated other methods

--- src/game/Game.java
+++ src/game/Game.java
@@ -6,59 +6,115 @@ import characters.*;
 import characters.Character;
 
 public class Game {
-	private Scanner input = new Scanner(System.in);
+	private Scanner input;
 	public Location location;
 	public String name;
-	public Player p1;
+	public Player player;
 	
+	public Game() {
+		this.input = new Scanner(System.in);
+	}
+
 	public void start() {
 		System.out.println("Adventure Game");
 		System.out.print("What is your name? Please enter the name: ");
 		name = input.next();
-		p1 = new Player(name);		
-		System.out.println(p1.getName()+ "welcome to the island");
+		player = new Player(name);		
+		System.out.println(player.getName()+ " welcome to the island");
 	}
 	
-	public void selectChar() {	
-		System.out.println("Which character you want to be?\n"
-				+ "-----------------------------------------\n"
-				+ "1-Samuria\tDamage:5\tHealth:21\tmoney:15\n"
-				+ "2-Archer\tDamage:7\tHealth:18\tmoney:20\n"
-				+ "3-Knight\tDamage:3\tHealth:8\tmoney:24");
-		
+	private void printChar() {
+		System.out.println("_________________________________________________________________\n"
+				+ "|\tWhich character you want to be? \t\t\t" + " " + "|\n"
+				+ "|\t1-Samuria\tDamage:5\tHealth:21\tmoney:15 |\n"
+				+ "|\t2-Archer\tDamage:7\tHealth:18\tmoney:20 |\n"
+				+ "|\t3-Knight\tDamage:3\tHealth:8\tmoney:24 |\n"
+				+ "|________________________________________________________________|");
+	}
+	
+	private void printLocation() {
+		System.out.println("________________________________________________________\n"
+				+ "|\tWhere dou you want to go? \t\t\t|\n"
+				+ "|\t1-Tool Store\tYou can buy weapon and armor\t|\n"
+				+ "|\t2-Safe House\tYour health will be restore.\t|\n"
+				+ "|_______________________________________________________|");
+	}
+	
+	
+	public void selectChar() {
+		printChar();
 		while (true) {
+			System.out.print("Enter the character name: ");
 			String charName = input.next();
 			
 			if(charName.equals("Samurai")) {
 				Character s = new Samurai();
-				p1.setDamage(s.getDamage());
-				p1.setHealth(s.getHealth());
-				p1.setWallet(s.getMoney());
-				System.out.println("You are Samurai anymore. You have " + p1.getDamage() + " damage " + p1.getHealth() + " health " + p1.getWallet()+ " money");
+				player.setId(s.getId());
+				player.setDamage(s.getDamage());
+				player.setHealth(s.getHealth());
+				player.setWallet(s.getMoney());
+				System.out.println("You are Samurai anymore. You have "
+							+ player.getDamage() + " damage " 
+							+ player.getHealth() + " health " 
+							+ player.getWallet()+ " money");
 				break;
 			}
 			
 			else if(charName.equals("Knight")) {
 				Knight k = new Knight();
-				p1.setDamage(k.getDamage());
-				p1.setHealth(k.getHealth());
-				p1.setWallet(k.getMoney());
-				System.out.println("You are Knight anymore. You have " + p1.getDamage() + " damage " + p1.getHealth() + " health " + p1.getWallet()+ " money");
+				player.setId(k.getId());
+				player.setDamage(k.getDamage());
+				player.setHealth(k.getHealth());
+				player.setWallet(k.getMoney());
+				System.out.println("You are Knight anymore. You have "
+						+ player.getDamage() + " damage " 
+						+ player.getHealth() + " health " 
+						+ player.getWallet()+ " money");
 				break;
 			}
 			
 			else if(charName.equals("Archer")) {
 				Archer a = new Archer();
-				p1.setDamage(a.getDamage());
-				p1.setHealth(a.getHealth());
-				p1.setWallet(a.getMoney());
-				System.out.println("You are Archer anymore. You have " + p1.getDamage() + " damage " + p1.getHealth() + " health " + p1.getWallet()+ " money");
+				player.setId(a.getId());
+				player.setDamage(a.getDamage());
+				player.setHealth(a.getHealth());
+				player.setWallet(a.getMoney());
+				System.out.println("You are Archer anymore. You have " 
+						+ player.getDamage() + " damage " 
+						+ player.getHealth() + " health " 
+						+ player.getWallet()+ " money");
 				break;
 			}
 			
 			else {
-				System.out.println("You entered the invalid character.Please try again.");
+				System.out.print("You entered the invalid character.Please try again:");
 			}	
-		}		
+		}
+	
+	}
+	
+	public void selectLocation() {
+		printLocation();
+		String locName = input.nextLine();
+		
+		while(true) {
+			System.out.print("Please enter the location:");
+			locName = input.nextLine();
+			
+			if(locName.equals("Tool Store")) {
+				location = new ToolStore(player,name);
+			} 
+			else if (locName.equals("Safe House")) {
+				location = new SafeHouse(player,name);
+			} 
+			else if (locName.equals("exit")) {
+				System.out.println("You are leaving the game.. See you soon");
+				System.exit(0);
+			} 
+			else {
+				System.out.println("Wrong location but you still are in the safe house");
+			}
+		}
 	}
 }
+
